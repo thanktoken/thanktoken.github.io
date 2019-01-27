d744a58c4c08e36964d9197876947edcb25502bf
Jörg Hohwiller <hohwille@users.sourceforge.net>
2019-01-27 15:47:07 +0100

    WIP

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  $(".main").onepage_scroll({
+    sectionContainer: "section",
+    easing: "linear",
+    animationTime: 600,
+    updateURL: false
+  });
+});
+
+function init() {
+  window.setTimeout(function() { start() }, 100);
+}
+
+function start() {
+	$('body').removeClass("loading").addClass('loaded');	
+}
+
+$(window).load(function() {
+	init();
+});
